e959612df03997e1a33cdbdd9294a814de6d7aff
Yutaka WATANABE <mail@ytn86.net>
2017-01-26 00:37:09 +0900

    change space width

--- pycap/util.py
+++ pycap/util.py
@@ -1,5 +1,5 @@
 def printHdr(name, value):
-    print('{0:20s}: {1}'.format(name, value))
+    print('{0:28s} : {1}'.format(name, value))
 
 def printHdrName(name):
     print('----------{}----------'.format(name))
